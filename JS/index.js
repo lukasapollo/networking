84a06210816ce2c0a040555f8ab45973e44a5694
luksouza <apolloboficial@gmail.com>
2022-04-19 19:58:00 -0300

    Feita a função de logout para autenticar novos usuarios no sistema

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBoOXcmz_-YWMH7OxY1JnDoG6Vy96RweM",
+  authDomain: "sdm-sistema.firebaseapp.com",
+  databaseURL: "https://sdm-sistema-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sdm-sistema",
+  storageBucket: "sdm-sistema.appspot.com",
+  messagingSenderId: "669496730661",
+  appId: "1:669496730661:web:8a05e09c1d0fc7555d0bcc"
+};
+
+firebase.initializeApp(firebaseConfig);
--- JS/logs.js
+++ JS/logs.js
@@ -1,16 +1,34 @@
 let count = 0;
 
+document.querySelector('.ctn-btn').addEventListener('click', (event) => {
+    event.preventDefault()
+    if(count == 0){
+        disabled()
+        Logout()
+    }
+    count++;
+})
+//
+function Logout() {
+    const userName = document.querySelector('#nameUser').value
+    const Email = document.querySelector('#sendEmail').value
+    const Password = document.querySelector('#sendPassword').value
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(Email, Password)
+    .then(() => {
+        disabled()
+        console.log('Cadastrado com sucesso!')
+    }).catch(err => {
+        console.log(err)
+        disabled()
+    })
+}
+
 function disabled(){
     const btnEntrar = document.querySelector('.btn-entrar')
     const btnLoader = document.querySelector('.loader-simples')
     btnEntrar.classList.toggle('disabled');
     btnLoader.classList.toggle('disabled');
 }
-
-document.querySelector('.ctn-btn').addEventListener('click', (event) => {
-    event.preventDefault()
-    if(count == 0){
-        disabled()
-    }
-    count++;
-})
